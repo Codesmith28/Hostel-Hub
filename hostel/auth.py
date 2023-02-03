6928b80f0ae22ac0a1897d757ad26cd2e7ba8778
Saumya40-codes <saumyabshah90@gmail.com>
2023-02-03 13:28:55 +0530

    user authentication nearly done

---
+++ hostel/auth.py
@@ -0,0 +1,54 @@
+from flask import Blueprint,render_template,request,flash,redirect,url_for
+from .models import User
+from werkzeug.security import generate_password_hash,check_password_hash
+from . import db
+auth = Blueprint('auth',__name__)
+
+
+@auth.route('/login',methods=['GET','POSTS'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        hostel = request.form.get('hostel')
+
+        user = User.query.filter_by(username=username).first()
+        if user:
+            if check_password_hash(user.password,password):
+                flash('Logged in successfully!',category='success')
+            else:
+                flash('Incorrect password!',category='error')
+        else:
+            flash('Username doesn\'t exist',category='error')
+    return render_template({{url_for('login')}})
+
+@auth.route('/logout')
+def logout():
+    return "logout"
+
+@auth.route('/signup',methods=['GET','POST'])
+def signup():
+    if(request.method == 'POST'):
+        username = request.form.get('username')
+        password = request.form.get('password')
+        hostel = request.form.get('hostel')
+
+        user = User.query.filter_by(username=username).first()
+
+        if user:
+            redirect (url_for('views.home'))
+            flash('Username already exists',category='error')
+
+        if len(username) < 1:
+            flash("Please enter a valid username.",category='error')
+        elif len(password) < 7:
+            flash("Please enter a password having length more than 7",category='error')
+        else:
+            new_user = User(username=username,password=generate_password_hash(password ,method='sha256'),hostel=hostel)
+            db.session.add(new_user)
+            db.session.commit()
+            redirect(url_for('views.home'))
+            flash('Account created',category='error')
+
+    return render_template("warden_register.html")
+
---
+++ hostel/views.py
@@ -0,0 +1,20 @@
+#we will store standard route of our website
+from flask import Blueprint,render_template
+
+views = Blueprint('views',__name__)
+
+@views.route('/')
+def home():
+    return render_template('profiles.html')
+
+@views.route('/hostelliteLogin')
+def hostellitelogin():
+    return render_template('login.html')
+
+@views.route('/wardenLogin')
+def wardenlogin():
+    return render_template('warden_login.html')
+
+@views.route('/wardenRegister')
+def wardenRegister():
+    return render_template('warden_register.html')
