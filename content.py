88811196ef1654593688ffbb663932ca70829fee
Sarthak Siddhpura <sarthaksiddhpura@gmail.com>
2023-02-18 13:16:35 +0530

    made warden dashboard a bit better

--- content.py
+++ content.py
@@ -3,4 +3,6 @@ from hostel import create_app
 app = create_app()
 
 if __name__ == '__main__':
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+    app.jinja_env.auto_reload = True
     app.run(debug=True)
